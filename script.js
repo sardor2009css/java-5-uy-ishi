633f1032a2b67e0693fbf8874d5f12ddf4c3cc07
sardor2009css <sardortoshtemirov621@gmail.com>
2025-08-09 13:35:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let name = prompt("Ismingizni kiriting:");
+let id = prompt("ID raqamingizni kiriting:");
+let password = prompt("Parolingizni kiriting:");
+
+let user = {
+  name: name,
+  id: id,
+  password: password
+};
+
+console.log(user);
+
